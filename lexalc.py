7c623ebb9e33d57007c21adb9304948568304268
procutepro <154824265+procutepro@users.noreply.github.com>
2025-08-09 17:54:25 +0530

    Update lexalc.py

--- lexalc.py
+++ lexalc.py
@@ -1,10 +1,1 @@
-import lexal
-import lexal.update as lexal
-import subprocess
-
-def doit():
-    lexal.directory = "C:\\Users\\user\\Downloads\\pratyushs_files\\VS_code\\frogassemblyc"
-    
-    lexal.compile()
-    
-    subprocess.run(["run.bat"])
+print("check this out")
